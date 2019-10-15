27b9ec83029dc5b624fc9be79000dbc61201665d
Jordan Halattre <jordan.halattre@hotmail.com>
2019-10-15 14:01:54 +0200

    Maj Accueil

--- application/views/accueil.php
+++ application/views/accueil.php
@@ -13,31 +13,27 @@
 </div>
 <div>
     <h2 class="colorblue">Nos catégories</h2>
-    <table>
-        <tr>
-            <td><a href=""><img class="imgcate" onmouseover="this.src='http://localhost/codei/assets/images/CATEGORIES ROLL OVER guitare.png'" onmouseout="this.src='http://localhost/codei/assets/images/CATEGORIES guitare.png'" src="http://localhost/codei/assets/images/CATEGORIES guitare.png" alt=""></a></td>
-            <td><a href=""><img class="imgcate" onmouseover="this.src='http://localhost/codei/assets/images/CATEGORIES ROLL OVER batterie.png'" onmouseout="this.src='http://localhost/codei/assets/images/CATEGORIES batterie.png'" src="http://localhost/codei/assets/images/CATEGORIES batterie.png" alt=""></a></td>
-            <td><a href=""><img class="imgcate" onmouseover="this.src='http://localhost/codei/assets/images/CATEGORIES ROLL OVER piano.png'" onmouseout="this.src='http://localhost/codei/assets/images/CATEGORIES piano.png'" src="http://localhost/codei/assets/images/CATEGORIES piano.png" alt=""></a></td>
-            <td><a href=""><img class="imgcate" onmouseover="this.src='http://localhost/codei/assets/images/CATEGORIES ROLL OVER micro.png'" onmouseout="this.src='http://localhost/codei/assets/images/CATEGORIES micro.png'" src="http://localhost/codei/assets/images/CATEGORIES micro.png" alt=""></a></td>
-        </tr>
-        <tr>
-            <td><a href=""><img class="imgcate" onmouseover="this.src='http://localhost/codei/assets/images/CATEGORIES ROLL OVER sono.png'" onmouseout="this.src='http://localhost/codei/assets/images/CATEGORIES sono.png'" src="http://localhost/codei/assets/images/CATEGORIES sono.png" alt=""></a></td>
-            <td><a href=""><img class="imgcate" onmouseover="this.src='http://localhost/codei/assets/images/CATEGORIES ROLL OVER cases.png'" onmouseout="this.src='http://localhost/codei/assets/images/CATEGORIES cases.png'" src="http://localhost/codei/assets/images/CATEGORIES cases.png" alt=""></a></td>
-            <td><a href=""><img class="imgcate" onmouseover="this.src='http://localhost/codei/assets/images/CATEGORIES ROLL OVER access.png'" onmouseout="this.src='http://localhost/codei/assets/images/CATEGORIES cable.png'" src="http://localhost/codei/assets/images/CATEGORIES cable.png" alt=""></a></td>
-            <td><a href=""><img class="imgcate" onmouseover="this.src='http://localhost/codei/assets/images/CATEGORIES ROLL OVER saxo.png'" onmouseout="this.src='http://localhost/codei/assets/images/CATEGORIES saxo.png'" src="http://localhost/codei/assets/images/CATEGORIES saxo.png" alt=""></a></td>
-        </tr>
-    </table>
+    <div class="row center">
+        <a href=""><img class="imgcate" onmouseover="this.src='http://localhost/codei/assets/images/CATEGORIES ROLL OVER guitare.png'" onmouseout="this.src='http://localhost/codei/assets/images/CATEGORIES guitare.png'" src="http://localhost/codei/assets/images/CATEGORIES guitare.png" alt=""></a>
+        <a href=""><img class="imgcate" onmouseover="this.src='http://localhost/codei/assets/images/CATEGORIES ROLL OVER batterie.png'" onmouseout="this.src='http://localhost/codei/assets/images/CATEGORIES batterie.png'" src="http://localhost/codei/assets/images/CATEGORIES batterie.png" alt=""></a>
+        <a href=""><img class="imgcate" onmouseover="this.src='http://localhost/codei/assets/images/CATEGORIES ROLL OVER piano.png'" onmouseout="this.src='http://localhost/codei/assets/images/CATEGORIES piano.png'" src="http://localhost/codei/assets/images/CATEGORIES piano.png" alt=""></a>
+        <a href=""><img class="imgcate" onmouseover="this.src='http://localhost/codei/assets/images/CATEGORIES ROLL OVER micro.png'" onmouseout="this.src='http://localhost/codei/assets/images/CATEGORIES micro.png'" src="http://localhost/codei/assets/images/CATEGORIES micro.png" alt=""></a>
+    </div>
+    <div class="row center">
+        <a href=""><img class="imgcate" onmouseover="this.src='http://localhost/codei/assets/images/CATEGORIES ROLL OVER sono.png'" onmouseout="this.src='http://localhost/codei/assets/images/CATEGORIES sono.png'" src="http://localhost/codei/assets/images/CATEGORIES sono.png" alt=""></a>
+        <a href=""><img class="imgcate" onmouseover="this.src='http://localhost/codei/assets/images/CATEGORIES ROLL OVER cases.png'" onmouseout="this.src='http://localhost/codei/assets/images/CATEGORIES cases.png'" src="http://localhost/codei/assets/images/CATEGORIES cases.png" alt=""></a>
+        <a href=""><img class="imgcate" onmouseover="this.src='http://localhost/codei/assets/images/CATEGORIES ROLL OVER access.png'" onmouseout="this.src='http://localhost/codei/assets/images/CATEGORIES cable.png'" src="http://localhost/codei/assets/images/CATEGORIES cable.png" alt=""></a>
+        <a href=""><img class="imgcate" onmouseover="this.src='http://localhost/codei/assets/images/CATEGORIES ROLL OVER saxo.png'" onmouseout="this.src='http://localhost/codei/assets/images/CATEGORIES saxo.png'" src="http://localhost/codei/assets/images/CATEGORIES saxo.png" alt=""></a>
+    </div>
 </div>
 <div id="divVentePart">
     <div>
         <h2 class="colorblue">Nos meilleures ventes</h2>
-        <table>
-            <tr>
-                <td><a href=""><img onmouseover="this.src='http://localhost/codei/assets/images/TOP VENTES ROLL OVER guitare.png'" onmouseout="this.src='http://localhost/codei/assets/images/TOP VENTES guitare.png'" src="http://localhost/codei/assets/images/TOP VENTES guitare.png" alt=""></a></td>
-                <td><a href=""><img class="imgvente" onmouseover="this.src='http://localhost/codei/assets/images/TOP VENTES ROLL OVER piano.png'" onmouseout="this.src='http://localhost/codei/assets/images/TOP VENTES piano.png'" src="http://localhost/codei/assets/images/TOP VENTES piano.png" alt=""></a></td>
-                <td><a href=""><img class="imgvente" onmouseover="this.src='http://localhost/codei/assets/images/TOP VENTES ROLL OVER saxo.png'" onmouseout="this.src='http://localhost/codei/assets/images/TOP VENTES saxo.png'" src="http://localhost/codei/assets/images/TOP VENTES saxo.png" alt=""></a></td>
-            </tr>
-        </table>
+        <div>
+            <a href=""><img onmouseover="this.src='http://localhost/codei/assets/images/TOP VENTES ROLL OVER guitare.png'" onmouseout="this.src='http://localhost/codei/assets/images/TOP VENTES guitare.png'" src="http://localhost/codei/assets/images/TOP VENTES guitare.png" alt=""></a>
+            <a href=""><img class="imgvente" onmouseover="this.src='http://localhost/codei/assets/images/TOP VENTES ROLL OVER piano.png'" onmouseout="this.src='http://localhost/codei/assets/images/TOP VENTES piano.png'" src="http://localhost/codei/assets/images/TOP VENTES piano.png" alt=""></a>
+            <a href=""><img class="imgvente" onmouseover="this.src='http://localhost/codei/assets/images/TOP VENTES ROLL OVER saxo.png'" onmouseout="this.src='http://localhost/codei/assets/images/TOP VENTES saxo.png'" src="http://localhost/codei/assets/images/TOP VENTES saxo.png" alt=""></a>
+        </div>
     </div>
     <div id="divpart">
         <h2 class="colorblue">Nos partenaires</h2>
